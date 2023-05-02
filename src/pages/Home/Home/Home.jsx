d90497a808ac5a96778d6353b83d6e22cd4643ba
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-05-02 09:39:35 +0600

    home page six card added with data

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,20 +1,71 @@
-import React from 'react';
-import { Button} from 'react-bootstrap';
-import ButtonToolBar from 'react-bootstrap/ButtonToolbar';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
+import { Link } from 'react-router-dom';
+import { Button, Card } from 'react-bootstrap';
+import { AiFillLike } from "react-icons/ai";
+
 
 const Home = () => {
+    const [chefData, setChefData] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/chefData')
+            .then(res => res.json())
+            .then(data => setChefData(data))
+            .catch(error => console.error(error))
+
+    }, [])
+    console.log(chefData)
     return (
 
-        < >
+        <div>
             <div className='bgImage'></div>
             <div className='content'>
                 <h2 className='text-white fw-bold fs-1'>The Best <span className='text-danger fs-1'>Food</span></h2>
                 <h2 className='fw-bold fs-4 text-white'>no matter how you cook, Recipe Roadmap has many the recipes.</h2>
                 <p className='text-white'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab accusantium quos, blanditiis sed sequi officiis delectus! Labore officiis, </p>
-                <button className='btn btn-danger'>Send Message</button>
+                <Link to="/contact" className='btn btn-danger'>Send Message</Link>
             </div>
-        </ >
+
+
+
+
+            {/* card */}
+
+
+            <div className='container'>
+                <h2 className='text-center mt-5'>Chef Detail</h2>
+                {
+                    <div className="row mb-5">
+                        {chefData[0]?.services?.map((service) => (
+                            <div className='col-12 col-md-6 col-lg-4 g-4'>
+                                <Card>
+                                    <Card.Img variant="top" src={service?.chef_picture} />
+                                    <Card.Body>
+                                        <Card.Title className='fw-bold fs-3 text-danger'>{service?.chef_name}</Card.Title>
+
+                                        <div className=''>
+                                            <p> Years of experience :{service?.years_of_experience}</p>
+                                            <p>Numbers of recipes :{service?.number_of_recipes}</p>
+                                        </div>
+                                        <div className='d-flex justify-content-between'>
+                                            <div className='d-flex'>
+
+                                                <AiFillLike className='fs-4'></AiFillLike>
+                                                <p className=' ms-2'>{service?.likes}</p>
+                                            </div>
+
+                                            <Button variant="primary">View Recipes</Button>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                }
+
+            </div>
+        </div>
     );
 };
 
